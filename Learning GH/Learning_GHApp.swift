deba4c9555538c86aa1a68ad4e2a846d15879c0a
awesomearjun <iamawesomearjun@gmail.com>
2021-05-08 21:34:46 -0400

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by A P on 2021-05-08.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
